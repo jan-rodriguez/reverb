50618b30d45a089a205a4270d3a7c2192b8db704
jkr241 <jkr241@mit.edu>
2014-04-14 03:13:41 -0400

    Added monster with basic AI to track player

---
+++ Assets/Scripts/AI/SetAITarget.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+
+	var monster : GameObject = GameObject.FindWithTag("Monster");
+	
+	monster.transform.SendMessage("SetTarget", gameObject, SendMessageOptions.DontRequireReceiver);
+
+}
+
+function Update () {
+
+}
---
+++ Assets/Scripts/AI/TheGoal.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var goal : Transform;
+
+function Start () {
+
+}
+
+function Update () {
+	
+	if(goal != null){
+		GetComponent(NavMeshAgent).destination = goal.position;
+	}
+
+}
+
+function SetTarget(target : GameObject){
+	goal = target.transform;
+}
