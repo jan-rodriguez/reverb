f55465675790b0cf03500af1ec6fce6a653f15a1
tyler <tlaprade@mit.edu>
2014-04-23 22:31:59 -0400

    can open and close door with E and Q, respectively

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,36 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+	
+	private bool isOpen;
+	
+	// Use this for initialization
+	void Start () {
+		isOpen = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("e")) {
+			this.OpenDoor();
+		}
+		if (Input.GetKeyDown("q")) {
+			this.CloseDoor();
+		}
+	}
+	
+	public void OpenDoor () {
+		if (!isOpen) {
+			transform.Translate(0,-100,0);
+			isOpen = true;
+		}
+	}
+	
+	public void CloseDoor () {
+		if (isOpen) {
+			transform.Translate(0,100,0);
+			isOpen = false;
+		}
+	}
+}
