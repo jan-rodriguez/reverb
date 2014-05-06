b81b5cfde6bdffc06447302e82372d904d403cb4
jkr241 <jkr241@mit.edu>
2014-05-05 21:54:25 -0400

    Reverted level load

--- Assets/Scripts/Trigger/WinScript.cs
+++ Assets/Scripts/Trigger/WinScript.cs
@@ -42,35 +42,36 @@ public class WinScript : MonoBehaviour {
 	}
 
 	void StartNextLevel () {
-		networkView.RPC ("LoadLevel", RPCMode.AllBuffered, "CityStage");
+//		networkView.RPC ("LoadLevel", RPCMode.AllBuffered, "CityStage");
+		Application.LoadLevel("CityStage");
 	}
 
-	[RPC]
-	IEnumerator LoadLevel (string level)
-	{
-		
-		// There is no reason to send any more data over the network on the default channel,
-		// because we are about to load the level, thus all those objects will get deleted anyway
-		Network.SetSendingEnabled(0, false);	
-		
-		// We need to stop receiving because first the level must be loaded first.
-		// Once the level is loaded, rpc's and other state update attached to objects in the level are allowed to fire
-		Network.isMessageQueueRunning = false;
-		
-		// All network views loaded from a level will get a prefix into their NetworkViewID.
-		// This will prevent old updates from clients leaking into a newly created scene.
-		Application.LoadLevel(level);
-		yield return null;
-		yield return null;
-		
-		// Allow receiving data again
-		Network.isMessageQueueRunning = true;
-		// Now the level has been loaded and we can start sending out data to clients
-		Network.SetSendingEnabled(0, true);
-		
-		
-//		foreach (GameObject go in GameObject.FindObjectsOfType(GameObject))
-//			go.SendMessage("OnNetworkLoadedLevel", SendMessageOptions.DontRequireReceiver);	
-	}
+//	[RPC]
+//	IEnumerator LoadLevel (string level)
+//	{
+//		
+//		// There is no reason to send any more data over the network on the default channel,
+//		// because we are about to load the level, thus all those objects will get deleted anyway
+//		Network.SetSendingEnabled(0, false);	
+//		
+//		// We need to stop receiving because first the level must be loaded first.
+//		// Once the level is loaded, rpc's and other state update attached to objects in the level are allowed to fire
+//		Network.isMessageQueueRunning = false;
+//		
+//		// All network views loaded from a level will get a prefix into their NetworkViewID.
+//		// This will prevent old updates from clients leaking into a newly created scene.
+//		Application.LoadLevel(level);
+//		yield return null;
+//		yield return null;
+//		
+//		// Allow receiving data again
+//		Network.isMessageQueueRunning = true;
+//		// Now the level has been loaded and we can start sending out data to clients
+//		Network.SetSendingEnabled(0, true);
+//		
+//		
+////		foreach (GameObject go in GameObject.FindObjectsOfType(GameObject))
+////			go.SendMessage("OnNetworkLoadedLevel", SendMessageOptions.DontRequireReceiver);	
+//	}
 
 }
