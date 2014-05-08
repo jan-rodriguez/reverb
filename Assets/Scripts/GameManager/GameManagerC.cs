39631a343c224727560cdca947101b2aac2cbec9
Nick Benson <nickjbenson@gmail.com>
2014-05-08 13:57:52 -0400

    Added missing scripts

---
+++ Assets/Scripts/GameManager/GameManagerC.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerC : MonoBehaviour {
+
+    public bool twoPlayers = false;
+    public bool pausedOrMenu = true;
+    public bool spawnedPlayer = false;
+
+    public NetworkManager networkManager;
+
+	// Use this for initialization
+    void Start() {
+	
+	}
+	
+	// Update is called once per frame
+    void Update() {
+
+        if (networkManager == null && GameObject.FindGameObjectWithTag("Player") != null) {
+            networkManager = (NetworkManager)GameObject.FindGameObjectWithTag("Player").GetComponent<NetworkManager>();
+        }
+
+        //If we are in city stage and already connected
+        if (Application.loadedLevelName == "CityStage" && (Network.isClient || Network.isServer) && !spawnedPlayer && networkManager != null) {
+            networkManager.SpawnPlayer();
+            pausedOrMenu = false;
+        }
+
+        if (!pausedOrMenu && Screen.lockCursor == false) {
+
+            Screen.showCursor = false;
+            Screen.lockCursor = true;
+
+        }
+	
+	}
+}
---
+++ Assets/Scripts/Trigger/CityWinScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CityWinScript : MonoBehaviour {
+
+    private float winTimer = 0f;
+    public int playersInside;
+
+    void Start() {
+        DontDestroyOnLoad(this);
+    }
+
+    void OnTriggerEnter(Collider collider) {
+
+        //Player walked into the platform
+        if (collider.transform.name == "FirstPersonController(Clone)") {
+            playersInside++;
+            //If both players are in the platform activate win animations
+            if (playersInside == (((GameManagerC)GameObject.FindGameObjectWithTag("GameManager").GetComponent("GameManagerC")).twoPlayers ? 2 : 1)) {
+                StartCoroutine("PlayWinAnimation");
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider collider) {
+        //Player left the platform
+        if (collider.transform.name == "FirstPersonController(Clone)") {
+            playersInside--;
+        }
+    }
+
+    IEnumerator PlayWinAnimation() {
+        while (winTimer < 1) {
+
+            winTimer += Time.deltaTime;
+            Debug.Log(winTimer);
+
+            yield return null;
+        }
+
+        Application.Quit();
+        Debug.Log("Quit attempted. Quitting only works in built projects, not from plays within the Unity Editor.");
+    }
+
+}
