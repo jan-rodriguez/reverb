4eec46498cb4148f978f6e94e8c899819670825a
jkr241 <jkr241@mit.edu>
2014-05-12 21:35:13 -0400

    Mic room now works

---
+++ Assets/MicSwap.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MicSwap : MonoBehaviour {
+
+	//Enable player lightflickering when entering
+	void OnTriggerEnter(Collider col){
+		if (col.transform.name == "FirstPersonController(Clone)") {
+			MicrophoneInput playerInput = col.GetComponentInChildren<MicrophoneInput>();
+			if(playerInput != null){
+				playerInput.enabled = true;
+			}
+		}
+	}
+
+	//Disable player light flickering
+	void OnTriggerExit(Collider col){
+		if (col.transform.name == "FirstPersonController(Clone)") {
+			MicrophoneInput playerInput = col.GetComponentInChildren<MicrophoneInput>();
+			if(playerInput != null){
+				playerInput.enabled = false;
+			}
+
+			Light playerLight = col.GetComponentInChildren<Light>();
+			playerLight.intensity = 1.0f;
+			playerLight.range = 50f;
+
+		}
+	}
+}
